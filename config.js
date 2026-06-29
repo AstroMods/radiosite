8a0e1d5fdf2280cd1d47481d82b46f34efe5d863
Vantix <117606560+AstroMods@users.noreply.github.com>
2026-06-29 12:48:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const CONFIG = {
 
     streamUrl: "http://51.255.235.165:2128/stream.mp3",
 
-    logo: "assets/logo.png",
-    favicon: "assets/favicon.ico",
+    logo: "https://i.ibb.co/G4YSyXFc/Chat-GPT-Image-Jun-29-2026-12-47-34-PM.png",
+    favicon: "https://i.ibb.co/G4YSyXFc/Chat-GPT-Image-Jun-29-2026-12-47-34-PM.png",
 
     discordInvite: "https://discord.com/invite/YaaEnHTSvk",
 
